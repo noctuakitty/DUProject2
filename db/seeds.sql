46f4f6c6cca7265f05a10215b3226884ce881696
ahffron <ahffron22@gmail.com>
2020-05-09 19:22:12 -0600

    schema.sql and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+insert into destinations (locationType,locationName,city) values ('Island','Hawaii','Honolulu');
+insert into destinations (locationType,locationName,city) values ('Island','Hawaii','Kailua');
+insert into destinations (locationType,locationName,city) VALUES ('Island','fiji','Labasa');
+insert into destinations (locationType,locationName,city) VALUES ('Island','fiji','suva');
+insert into destinations (locationType,locationName,city) VALUES ('Island','bahamas','Freeport');
+insert into destinations (locationType,locationName,city) VALUES ('Island','bahamas','nassau');
+insert into destinations (locationType,locationName,city) VALUES ('Mountain','United States','Breckenridge');
+insert into destinations (locationType,locationName,city) VALUES ('Mountain','Switzerland', 'Zermatt');
+insert into destinations (locationType,locationName,city) VALUES ('Mountain','Argentina', 'Las Lenas');
+insert into destinations (locationType,locationName,city) VALUES ('Mountain','United States','Aspen');
+insert into destinations (locationType,locationName,city) VALUES ('Mountain','Switzerland','St. Moritz');
+insert into destinations (locationType,locationName,city) VALUES ('Historic','Peru','Machu Picchu');
+insert into destinations (locationType,locationName,city) VALUES ('Historic','Britain','Stonehenge');
+insert into destinations (locationType,locationName,city) VALUES ('Historic','Greece','Acropolis');
+insert into destinations (locationType,locationName,city) VALUES ('Historic','Italy','Rome');
